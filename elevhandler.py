af8e5433032c0ac039b0e46a0f87f42d62706d6d
Alicia Rimmi <alri1020@storsjogymnasiet.se>
2023-05-09 08:56:34 +0200

    first commit

---
+++ elevhandler.py
@@ -0,0 +1,11 @@
+import elev
+
+class elevhandler:
+
+    def __init__(self):
+        self.elevlist = []
+
+    def print_meny(self):
+        print("-----------------------")
+        print("\n\t\t\tMENY\n")
+        print()
---
+++ main.py
@@ -0,0 +1,14 @@
+import elevhandler
+
+looping = True
+
+skola = elevhandler.elevhandler()
+
+skola.print_meny()
+
+while looping:
+    val = 2
+    print("hej")
+    
+    if (val == 2):
+        break
